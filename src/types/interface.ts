8135b838dee79a694022aca5a16182299f6ab52e
HewkawAr <hewkawar@gmail.com>
2025-04-15 14:41:21 +0700

    Update interface.ts

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,9 +1,9 @@
-export interface twSuccessResponse {
+export interface twDataResponse {
     status: {
         message: string;
-        code: "SUCCESS";
+        code: twResponseStatus;
     };
-    data: {
+    data?: {
         voucher: Voucher,
         owner_profile: {
             full_name: string,
@@ -14,91 +14,7 @@ export interface twSuccessResponse {
     }
 }
 
-export interface twErrorCannotGetOwnVoucherResponse {
-    status: {
-        message: string;
-        code: "CANNOT_GET_OWN_VOUCHER";
-    };
-    data: {
-        voucher: Voucher,
-        owner_profile: {
-            full_name: string,
-        },
-        my_ticket: {
-            amount_baht: string,
-        }
-    }
-}
-
-export interface twErrorTargetUserNotFoundResponse {
-    status: {
-        message: string;
-        code: "TARGET_USER_NOT_FOUND";
-    };
-    data: {
-        voucher: Voucher,
-        owner_profile: {
-            full_name: string,
-        },
-        my_ticket: {
-            amount_baht: string,
-        }
-    }
-}
-
-export interface twErrorInternalErrorResponse {
-    status: {
-        message: string;
-        code: "INTERNAL_ERROR";
-    };
-    data: null;
-}
-
-export interface twErrorVoucherOutOfStockResponse {
-    status: {
-        message: string;
-        code: "VOUCHER_OUT_OF_STOCK";
-    };
-    data: {
-        voucher: Voucher,
-        owner_profile: {
-            full_name: string,
-        },
-        my_ticket: {
-            amount_baht: string,
-        }
-    };
-}
-
-export interface twErrorVoucherNotFoundResponse {
-    status: {
-        message: string;
-        code: "VOUCHER_NOT_FOUND";
-    };
-    data: null;
-}
-
-export interface twErrorVoucherExpiredResponse {
-    status: {
-        message: string;
-        code: "VOUCHER_EXPIRED";
-    };
-    data: {
-        voucher: Voucher,
-        owner_profile: {
-            full_name: string,
-        },
-        my_ticket: {
-            amount_baht: string,
-        }
-    }
-}
-
-export type twErrorResponse = twErrorCannotGetOwnVoucherResponse | twErrorTargetUserNotFoundResponse | twErrorInternalErrorResponse | twErrorVoucherOutOfStockResponse | twErrorVoucherNotFoundResponse | twErrorVoucherExpiredResponse;
-
-export interface NetworkErrorResponse {
-    error: "Network or server error. Please try again later."
-};
+export type twResponseStatus = "SUCCESS" | "CANNOT_GET_OWN_VOUCHER" | "TARGET_USER_NOT_FOUND" | "INTERNAL_ERROR" | "VOUCHER_OUT_OF_STOCK" | "VOUCHER_NOT_FOUND" | "VOUCHER_EXPIRED";
 
 export interface Voucher {
     voucher_id: string
@@ -114,4 +30,4 @@ export interface Voucher {
     available: number
 };
 
-export type twResponse = twSuccessResponse | twErrorResponse | NetworkErrorResponse;
+export type twResponse = twDataResponse;
