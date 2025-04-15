260c6a3658e8dc4786ac793c5da1005e870f4365
HewkawAr <hewkawar@gmail.com>
2025-04-15 14:36:57 +0700

    Add Example

---
+++ example/module.js
@@ -0,0 +1,30 @@
+import { twApi } from "truewalletvoucher";
+
+async function redeem(code) {
+    const voucher = await twApi(code, '[เบอร์โทร]');
+    switch (voucher.status.code) {
+        case "SUCCESS":
+            console.log('ไดรับเงินแล้วจำนวน' + voucher.data.my_ticket.amount_baht)
+            break;
+        case "CANNOT_GET_OWN_VOUCHER":
+            console.log('รับซองตัวเองไม่ได้')
+            break;
+        case "TARGET_USER_NOT_FOUND":
+            console.log('ไม่พบเบอร์นี้ในระบบ')
+            break;
+        case "INTERNAL_ERROR":
+            console.log('ไม่ซองนี้ในระบบ หรือ URL ผิด')
+            break;
+        case "VOUCHER_OUT_OF_STOCK":
+            console.log('มีคนรับไปแล้ว')
+            break;
+        case "VOUCHER_NOT_FOUND":
+            console.log('ไม่พบซองในระบบ')
+            break;
+        case "VOUCHER_EXPIRED":
+            console.log('ซองวอเลทนี้หมดอายุแล้ว')
+            break;
+        default:
+            break;
+    }
+}
